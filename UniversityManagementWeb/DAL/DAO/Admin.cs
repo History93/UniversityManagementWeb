de3d12aea8d97b4d363e81ed17a9e59236384aeb
history93 <bhuiyanrobin93@gmail.com>
2015-04-23 21:25:26 -0700

    Main File
    Signed-off-by: history93 <bhuiyanrobin93@gmail.com>

---
+++ UniversityManagementWeb/DAL/DAO/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityManagementWeb.DAL.DAO
+{
+    public class Admin
+    {
+        private int Id{set;get;}
+        private int UserName { set; get; }
+        public int Password { set; get; }
+    }
+}
---
+++ UniversityManagementWeb/DAL/DAO/Faculty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityManagementWeb.DAL.DAO
+{
+    public class Faculty:Admin
+    {
+        public int Id { set; get; }
+        public string FacultyName { set; get; }
+        public string FacultyCode { set; get; }
+        public string FacultyEmail { set; get; }
+        
+    }
+}
